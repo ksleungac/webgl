34e63935d20ff6bf64d288f55ed292c9d9db40ba
oscarleung <kingsuen.leung@hk.yusen-logistics.com>
2023-07-21 11:27:45 +0800

    3D rotating cube

--- main.js
+++ main.js
@@ -5,17 +5,70 @@ if (!gl) {
   throw new Error('No WebGL for you suckers')
 }
 
+const { mat4 } = glMatrix;
+
 const vertexData = [
-  0, 1, 0,
-  1, -1, 0,
-  -1, -1, 0
+  // Front
+  0.5, 0.5, 0.5,
+  0.5, -.5, 0.5,
+  -.5, 0.5, 0.5,
+  -.5, 0.5, 0.5,
+  0.5, -.5, 0.5,
+  -.5, -.5, 0.5,
+
+  // Left
+  -.5, 0.5, 0.5,
+  -.5, -.5, 0.5,
+  -.5, 0.5, -.5,
+  -.5, 0.5, -.5,
+  -.5, -.5, 0.5,
+  -.5, -.5, -.5,
+
+  // Back
+  0.5, 0.5, -.5,
+  0.5, -.5, -.5,
+  -.5, 0.5, -.5,
+  -.5, 0.5, -.5,
+  0.5, -.5, -.5,
+  -.5, -.5, -.5,
+
+  // Right
+  0.5, 0.5, 0.5,
+  0.5, -.5, 0.5,
+  0.5, 0.5, -.5,
+  0.5, 0.5, -.5,
+  0.5, -.5, 0.5,
+  0.5, -.5, -.5,
+
+  // Top
+  0.5, 0.5, 0.5,
+  0.5, 0.5, -.5,
+  -.5, 0.5, 0.5,
+  -.5, 0.5, 0.5,
+  0.5, 0.5 ,-.5,
+  -.5, 0.5, -.5,
+
+  // Bottom
+  0.5, -.5, 0.5,
+  0.5, -.5, -.5,
+  -.5, -.5, 0.5,
+  -.5, -.5, 0.5,
+  0.5, -.5 ,-.5,
+  -.5, -.5, -.5,
 ];
 
-const colorData = [
-  1, 0, 0,
-  0, 1, 0,
-  0, 0, 1
-];
+
+function randomColor() {
+  return [Math.random(), Math.random(), Math.random()];
+}
+ 
+let colorData = []
+for (let face = 0; face < 6; face++) {
+  let faceColor = randomColor();
+  for (let vertex = 0; vertex < 6; vertex++) {
+    colorData.push(...faceColor);
+  }
+}
 
 const positionBuffer = gl.createBuffer()
 gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
@@ -33,9 +86,11 @@ attribute vec3 position;
 attribute vec3 color;
 varying vec3 vColor;
 
+uniform mat4 matrix; 
+
 void main() {
   vColor = color;
-  gl_Position = vec4(position, 1);
+  gl_Position = matrix * vec4(position, 1);
 }
 `);
 gl.compileShader(vertexShader);
@@ -68,4 +123,22 @@ gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
 gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
 
 gl.useProgram(program);
-gl.drawArrays(gl.TRIANGLES, 0, 3);
+gl.enable(gl.DEPTH_TEST);
+
+const uniformLocations = {
+  matrix: gl.getUniformLocation(program, `matrix`),
+};
+
+const matrix = mat4.create();
+mat4.translate(matrix, matrix, [.2, .5, 0]);
+mat4.scale(matrix, matrix, [0.25, 0.25, 0.25]);
+
+function animate() {
+  requestAnimationFrame(animate);
+  mat4.rotateZ(matrix, matrix, Math.PI / 180);
+  mat4.rotateX(matrix, matrix, Math.PI / 180);
+  gl.uniformMatrix4fv(uniformLocations.matrix, false, matrix);
+  gl.drawArrays(gl.TRIANGLES, 0, vertexData.length / 3);
+}
+
+animate();
